455a2d9ddd77d18132b0e4b9634780a126d5aded
teetham <84436954+teetham@users.noreply.github.com>
2023-05-27 12:37:12 +0700

    firstCommit

---
+++ json.js
@@ -0,0 +1,335 @@
+{
+    "type": "bubble",
+    "size": "giga",
+    "body": {
+      "type": "box",
+      "layout": "vertical",
+      "contents": [
+        {
+          "type": "text",
+          "text": "ใบแจ้งค่าเช่าห้อง",
+          "weight": "bold",
+          "color": "#00AB00",
+          "size": "xl",
+          "style": "normal",
+          "align": "center"
+        },
+        {
+          "type": "text",
+          "text": "ห้อง 1111",
+          "weight": "bold",
+          "size": "xl",
+          "margin": "none",
+          "align": "center",
+          "color": "#0033FF"
+        },
+        {
+          "type": "box",
+          "layout": "horizontal",
+          "contents": [
+            {
+              "type": "text",
+              "text": "ทวีชัย 1",
+              "size": "lg",
+              "color": "#000000",
+              "wrap": true,
+              "align": "start",
+              "weight": "regular"
+            },
+            {
+              "type": "text",
+              "text": "ม.ค. 65",
+              "size": "lg",
+              "color": "#000000",
+              "wrap": true,
+              "align": "end",
+              "weight": "regular"
+            }
+          ],
+          "position": "relative",
+          "margin": "xl",
+          "spacing": "none"
+        },
+        {
+          "type": "box",
+          "layout": "vertical",
+          "margin": "sm",
+          "spacing": "sm",
+          "contents": [
+            {
+              "type": "separator",
+              "margin": "md"
+            },
+            {
+              "type": "box",
+              "layout": "horizontal",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "ค่าเช่าห้อง",
+                  "size": "sm",
+                  "flex": 0,
+                  "margin": "none",
+                  "color": "#000000"
+                },
+                {
+                  "type": "text",
+                  "text": "1,800 บาท",
+                  "size": "sm",
+                  "color": "#000000",
+                  "align": "end"
+                }
+              ],
+              "margin": "lg"
+            },
+            {
+              "type": "separator",
+              "margin": "lg"
+            }
+          ]
+        },
+        {
+          "type": "box",
+          "layout": "vertical",
+          "margin": "none",
+          "spacing": "xs",
+          "contents": [
+            {
+              "type": "box",
+              "layout": "horizontal",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "ค่าน้ำ",
+                  "size": "sm",
+                  "color": "#000000",
+                  "flex": 0,
+                  "margin": "none"
+                },
+                {
+                  "type": "text",
+                  "text": "125 บาท",
+                  "size": "sm",
+                  "color": "#000000",
+                  "align": "end"
+                }
+              ],
+              "margin": "sm"
+            },
+            {
+              "type": "box",
+              "layout": "horizontal",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "(3427 - 3422) x 25",
+                  "size": "sm",
+                  "color": "#000000",
+                  "flex": 0,
+                  "margin": "none"
+                }
+              ],
+              "margin": "none"
+            },
+            {
+              "type": "separator",
+              "margin": "sm"
+            }
+          ]
+        },
+        {
+          "type": "box",
+          "layout": "vertical",
+          "margin": "none",
+          "spacing": "xs",
+          "contents": [
+            {
+              "type": "box",
+              "layout": "horizontal",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "ค่าไฟ",
+                  "size": "sm",
+                  "color": "#000000",
+                  "flex": 0,
+                  "margin": "none"
+                },
+                {
+                  "type": "text",
+                  "text": "624 บาท",
+                  "size": "sm",
+                  "color": "#000000",
+                  "align": "end"
+                }
+              ],
+              "margin": "sm"
+            },
+            {
+              "type": "box",
+              "layout": "horizontal",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "(8237 - 8159) x 8",
+                  "size": "sm",
+                  "color": "#000000",
+                  "flex": 0,
+                  "margin": "none"
+                }
+              ],
+              "margin": "none"
+            },
+            {
+              "type": "separator",
+              "margin": "sm"
+            }
+          ]
+        },
+        {
+          "type": "box",
+          "layout": "vertical",
+          "margin": "none",
+          "spacing": "sm",
+          "contents": [
+            {
+              "type": "box",
+              "layout": "horizontal",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "ค่าส่วนกลาง",
+                  "size": "sm",
+                  "flex": 0,
+                  "margin": "none",
+                  "color": "#000000"
+                },
+                {
+                  "type": "text",
+                  "text": "250 บาท",
+                  "size": "sm",
+                  "color": "#000000",
+                  "align": "end"
+                }
+              ],
+              "margin": "lg"
+            },
+            {
+              "type": "separator",
+              "margin": "lg"
+            }
+          ]
+        },
+        {
+          "type": "box",
+          "layout": "vertical",
+          "margin": "none",
+          "spacing": "sm",
+          "contents": [
+            {
+              "type": "box",
+              "layout": "horizontal",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "อื่นๆ (ค่าแนะนำ)",
+                  "size": "sm",
+                  "flex": 0,
+                  "margin": "none",
+                  "color": "#000000"
+                },
+                {
+                  "type": "text",
+                  "text": "-200 บาท",
+                  "size": "sm",
+                  "color": "#000000",
+                  "align": "end"
+                }
+              ],
+              "margin": "lg"
+            },
+            {
+              "type": "separator",
+              "margin": "lg"
+            }
+          ]
+        },
+        {
+          "type": "box",
+          "layout": "vertical",
+          "margin": "none",
+          "spacing": "sm",
+          "contents": [
+            {
+              "type": "box",
+              "layout": "horizontal",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "รวม :",
+                  "size": "xl",
+                  "flex": 0,
+                  "margin": "none",
+                  "color": "#FF0000",
+                  "weight": "regular"
+                },
+                {
+                  "type": "text",
+                  "text": "2599  บาท",
+                  "size": "xl",
+                  "color": "#FF0000",
+                  "align": "end",
+                  "weight": "regular"
+                }
+              ],
+              "margin": "lg"
+            },
+            {
+              "type": "separator",
+              "margin": "lg"
+            }
+          ]
+        },
+        {
+          "type": "box",
+          "layout": "vertical",
+          "margin": "none",
+          "spacing": "sm",
+          "contents": [
+            {
+              "type": "box",
+              "layout": "vertical",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "โอนเข้าบัญชี : ลักษณาวดี",
+                  "size": "lg",
+                  "color": "#000000",
+                  "wrap": true,
+                  "align": "center",
+                  "weight": "bold",
+                  "margin": "none"
+                },
+                {
+                  "type": "text",
+                  "text": "ไทยพาณิชย์ : 111111111111",
+                  "size": "lg",
+                  "color": "#000000",
+                  "wrap": true,
+                  "align": "center",
+                  "weight": "bold",
+                  "margin": "sm"
+                }
+              ],
+              "margin": "xl"
+            }
+          ]
+        }
+      ]
+    },
+    "styles": {
+      "footer": {
+        "separator": true
+      }
+    }
+  }
